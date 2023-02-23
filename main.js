c9c36477fc953da4729974c68338d7800e48c119
Mariami-N <avetianimari@gmail.com>
2023-02-22 22:07:51 -0800

    price range cont3

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-let contaner=document.querySelector(".contaner")
+let container=document.querySelector(".container")
 let products = [["Apple", 2.4], ["Banana", 1.8], ["Cherry", 3], ["Peach", 1.2], ["Watermelon", 0.7], ["Melon", 1], ["Plum", 3.2]]
     
+let min1 = document.querySelector(".min1")
+let max1 = document.querySelector(".max1")
+
+let boxes = document.createElement("div")
+container.appendChild(boxes)
+boxes.classList.add("boxes")
+
+
+let prod
 
     products.forEach((prod) => {
 
     let box = document.createElement("div");
     box.classList.add("box");
-    contaner.appendChild(box);
+    boxes.appendChild(box);
 
     let fruit = document.createElement("h1");
     box.appendChild(fruit);
@@ -30,16 +39,40 @@ let products = [["Apple", 2.4], ["Banana", 1.8], ["Cherry", 3], ["Peach", 1.2], 
 low.addEventListener("click", function (){
     products.sort((a, b) => a[1] - b[1]);
     console.log(products)
+    boxes.innerHTML=" "
+
+    products.forEach(prod => {
+        let box2 = document.createElement("div");
+        box2.classList.add("box2")
+        boxes.appendChild(box2);
+        box2.innerHTML = `<h1>${prod[0]}</h1><p>${prod[1]}</p>`;
+      });
 })
  
 high.addEventListener("click", function (){
     products.sort((a,b) => b[1]- a[1]);
     console.log(products)
-})
+    boxes.innerHTML=" "
+
+    products.forEach(prod => {
+        let box2 = document.createElement("div");
+        box2.classList.add("box2")
+        boxes.appendChild(box2);
+        box2.innerHTML = `<h1>${prod[0]}</h1><p>${prod[1]}</p>`;
+    })
+});
 
 az.addEventListener("click", function (){    
     products.sort();
     console.log(products)
+    boxes.innerHTML=" "
+
+    products.forEach(prod => {
+        let box2 = document.createElement("div");
+        box2.classList.add("box2")
+        boxes.appendChild(box2);
+        box2.innerHTML = `<h1>${prod[0]}</h1><p>${prod[1]}</p>`;
+    })
 })
 
 za.addEventListener("click", function() {
@@ -48,23 +81,22 @@ za.addEventListener("click", function() {
         if (a > b) return -1; 
         return 0; 
     }); 
-      console.log(products); 
+    boxes.innerHTML=" "
+
+    products.forEach(prod => {
+        let box2 = document.createElement("div");
+        box2.classList.add("box2")
+        boxes.appendChild(box2);
+        box2.innerHTML = `<h1>${prod[0]}</h1><p>${prod[1]}</p>`;
+    })
     });
-
-filter.addEventListener("click", function(){
-    // products.filter(function(x){ return x.Price >= 0.7 && x.Price <= 3.2})
-    console.log(products)
-    
-})
      
-    
-
-
-    
-    
-
-    
-   
-
-
-
+                filter.addEventListener("click", function () {
+                    boxes.innerHTML = " ";
+                let filtered = products.filter((smth) => smth[1] >= min1.value && smth[1]<=max1.value);
+                filtered.forEach(prod => {
+                    let box2 = document.createElement("div");
+                    box2.classList.add("box2");
+                    boxes.appendChild(box2);
+                    box2.innerHTML = `<h1>${prod[0]}</h1><p>${prod[1]}</p>`;})
+                })
